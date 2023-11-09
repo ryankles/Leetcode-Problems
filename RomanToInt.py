8ce089821659293c1c52942eecb262f57c641473
Ryan Reeves <122695617+ryanreevess@users.noreply.github.com>
2023-11-08 17:34:04 -0800

    bruh

--- RomanToInt.py
+++ RomanToInt.py
@@ -1,15 +1,21 @@
 class Solution(object):
-    s = ("MCMXCIVM")
+    s = ("MCMXCIV")
     def romanToInt(s):
         numeralDict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         finalVal = 0
-        for i in range(len(s)):
+        for i in range(len(s)-1):
             if s[i] in numeralDict:
-                if s[i] == "I" or "X" or "C" and len(s) != i:
-                        ++i
+                if numeralDict[s[i]] < numeralDict[s[i+1]]:
                         finalVal = finalVal + (numeralDict[s[i+1]] - numeralDict[s[i]])
+                        print(f"If value: {numeralDict[s[i]]} \nPrintval: {finalVal}")
+                        print(i)
+                        break
                 else:
                     finalVal = finalVal + numeralDict[s[i]]
-            ++i
+                    print(f"Else value: {numeralDict[s[i]]} \nPrintval: {finalVal}")
+                    print(i)
+
+
+        #finalVal = finalVal + numeralDict[s[i]]
         print(finalVal)
     romanToInt(s)
