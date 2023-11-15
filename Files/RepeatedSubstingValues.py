1cbc784f839a3c5b5847d3dec04965b8e27fa3bd
Ryan Reeves <122695617+ryanreevess@users.noreply.github.com>
2023-11-15 14:52:12 -0800

    Finished RepeatedSubstring

--- Files/RepeatedSubstingValues.py
+++ Files/RepeatedSubstingValues.py
@@ -1,19 +1,20 @@
 class Solution(object):
-    s = 'abab'
-    def repeatedSubstringPattern(s):
+    s = 'a'
+    self = True
+    def repeatedSubstringPattern(self, s):
         newString = s[1:len(s)] + s[0]
-        print(newString)
-        for i in range(len(s)-1):
-            if newString != s:
-                newString += newString[0]
-                newString = newString[1:len(newString)]
-                print(newString)
-                print('false')
-                return False
-            else:       
-                print('true')
-                return True
+        if len(s) == 1:
+            self = True
+        else:
+            for i in range(len(s)-1):
+                if newString != s:
+                    newString += newString[0]
+                    newString = newString[1:len(newString)]
+                    self = False
+                else:       
+                    self = True
+        print(self)
                 
             
 
-    repeatedSubstringPattern(s)
+    repeatedSubstringPattern(self, s)
